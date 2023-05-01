1a4ca6421d1d334bfa96e00e411594523ec061f6
Brian Morse <brian.p.morse@gmail.com>
2023-04-30 20:27:40 -0700

    completed schema and seed files

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,41 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+-- Create Tables for seed data
+DROP TABLE IF EXISTS category;
+CREATE TABLE category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS product;
+CREATE TABLE product (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  tag_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS product_tag;
+CREATE TABLE product_tag (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  product_id INTEGER,
+  tag_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES product (id),
+  FOREIGN KEY (tag_id) REFERENCES tag (id)
+);
+
---
+++ db/seed.sql
@@ -0,0 +1,34 @@
+-- Seed data for `category` table
+INSERT INTO category (category_name) VALUES 
+('accessories'),
+('electronics'),
+('home goods'),
+('furniture'),
+('clothing');
+
+-- Seed data for `product` table
+INSERT INTO product (product_name, price, stock, category_id) VALUES 
+('watch', 100.00, 20, 1),
+('headphones', 50.00, 15, 2),
+('lamp', 25.00, 30, 3),
+('couch', 500.00, 5, 4),
+('t-shirt', 20.00, 25, 5);
+
+-- Seed data for `product_tag` table
+INSERT INTO product_tag (product_id, tag_id) VALUES 
+(1, 1),
+(1, 2),
+(2, 2),
+(2, 4),
+(3, 4),
+(4, 1),
+(4, 4),
+(5, 5);
+
+-- Seed data for `tag` table
+INSERT INTO tag (tag_name) VALUES 
+('accessory'),
+('electronic'),
+('home good'),
+('furniture'),
+('clothing');
